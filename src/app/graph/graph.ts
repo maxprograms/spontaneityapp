5928e314fea45a8abb44ef81901767193909a397
RunDMC2 <dylan.cygul@gmail.com>
2026-03-06 14:43:28 -0500

    bare graph implementation skeleton

---
+++ src/app/graph/graph.ts
@@ -0,0 +1,24 @@
+
+
+interface Location{
+    name: string;
+    lat: number;
+    lng: number;
+}
+
+interface Path{
+    loc_A: Location;
+    loc_B: Location;
+    distance: number;
+}
+
+class Graph {
+
+    constructor(){
+
+    }
+
+    add_node():void{}
+    remove_node():void{}
+    
+}
