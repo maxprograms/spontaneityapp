8f342dcf9a36270c797d6340f1e6baa1c713bcb2
RunDMC2 <dylan.cygul@gmail.com>
2026-03-09 12:47:27 -0400

    added more methods and removed Path interface

--- src/app/graph/graph.ts
+++ src/app/graph/graph.ts
@@ -7,12 +7,6 @@ interface Location{
     lng: number;
 }
 
-interface Path{
-    loc_A: Location;
-    loc_B: Location;
-    distance: number;
-}
-
 class Graph {
 
     private locations: Map<string, Location>;
@@ -51,16 +45,50 @@ class Graph {
             if (i === undefined){
                 throw new Error("Location not found");
             }
-            
+
             const distance = this.calculate_distance(old_loc, location);
             this.matrix[i]![new_index] = distance;
             this.matrix[new_index]![i] = distance;
         }
 
     }
-    remove_node():void{}
 
-    calculate_distance(locA: Location, locB: Location): number{
+    remove_node(id: string):void{
+        if (!this.locations.has(id)){
+            throw new Error("Location not found");
+        } 
+
+         const remove_index = this.locations_index.get(id)!;
+
+        this.locations.delete(id);
+        this.locations_index.delete(id);
+
+        this.matrix.splice(remove_index, 1);
+        for (const row of this.matrix) {
+            row.splice(remove_index, 1);
+        }
+
+        for (const [old_id, index] of this.locations_index) {
+            if (index > remove_index) {
+            this.locations_index.set(old_id, index - 1);
+            }
+        }
+    }
+
+    get_location(id: string): Location | undefined {
+        return this.locations.get(id);
+    }
+
+    get_distance(from_id: string, to_id: string): number {
+        const i = this.locations_index.get(from_id);
+        const j = this.locations_index.get(to_id);
+        if (i === undefined || j === undefined) {
+            throw new Error("One or both locations not found");
+        }
+    return this.matrix[i]![j]!;
+    }
+
+    private calculate_distance(locA: Location, locB: Location): number{
         const dx = locB.lat - locA.lat;
         const dy = locB.lng - locA.lng;
         return Math.sqrt(dx * dx + dy * dy);
