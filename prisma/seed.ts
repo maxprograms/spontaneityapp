7adccebc304b3dbabd7064a495de3ebe2f389d9c
maxprograms <maxpreble@gmail.com>
2026-03-30 22:24:08 -0400

    Seed script for DB locations as well as generated prisma files

---
+++ prisma/seed.ts
@@ -0,0 +1,43 @@
+import { PrismaClient } from "../generated/prisma";
+
+const db = new PrismaClient();
+
+async function seedLocations() {
+  const res = await fetch("https://campusmap.ufl.edu/assets/boundaries.json");
+  const data = await res.json() as { features: any[] };
+
+  const locations = data.features
+    .map((feature: any) => ({
+      id: feature.properties.PropSTCode as string,
+      name: feature.properties.PropName as string,
+      latitude: feature.properties.Latitude as number,
+      longitude: feature.properties.Longitude as number,
+    }))
+    .filter(
+      (loc) =>
+        loc.id &&
+        loc.name &&
+        loc.latitude != null &&
+        loc.longitude != null
+    );
+
+  const result = await db.location.createMany({
+    data: locations,
+    skipDuplicates: true,
+  });
+
+  console.log(`Seeded ${result.count} locations.`);
+}
+
+async function main() {
+  await seedLocations();
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await db.$disconnect();
+  });
