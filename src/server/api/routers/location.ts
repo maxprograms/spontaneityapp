bdd3b17cc833b8402721d43e10ef7ca0c7ce3e00
John Ronchetto <john4.col@gmail.com>
2026-04-17 17:35:04 -0400

    fixed path bug

--- src/server/api/routers/location.ts
+++ src/server/api/routers/location.ts
@@ -1,6 +1,6 @@
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 import { z } from "zod";
-import { getAllLocations } from "~/server/api/queries/locationQueries";
+import { getAllLocations } from "~/server/queries/locationQueries";
 
 export const locationRouter = createTRPCRouter({
     getLocation: publicProcedure
